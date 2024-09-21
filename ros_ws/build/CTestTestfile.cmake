df70332eb0f4e1797abf6c9c414d72373766da81
Vardan <vardanmittal000@gmail.com>
2024-09-21 13:02:04 +0530

    starting OWL_Sorting project

---
+++ ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vardan20/OWL_Sorting/ros_ws/src
+# Build directory: /home/vardan20/OWL_Sorting/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("owl_sim")
