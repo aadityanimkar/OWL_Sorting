2bbbcb58403a3bc637f72f0e3bb5b0e7bd505252
Vardan <vardanmittal000@gmail.com>
2024-09-21 17:20:29 +0530

    making things work again

--- ros_wk/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vardan20/OWL_Sorting/ros_wk/src
-# Build directory: /home/vardan20/OWL_Sorting/ros_wk/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("owl_robot")
--- ros_wk/build/owl_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vardan20/OWL_Sorting/ros_wk/src/owl_robot
-# Build directory: /home/vardan20/OWL_Sorting/ros_wk/build/owl_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
